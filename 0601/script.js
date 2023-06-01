56689b0e773d5f67c04eea235dc7ad70a3a88d23
goeunjib <goeunji0948@naver.com>
2023-06-01 17:05:51 +0900

    0601

---
+++ 0601/script.js
@@ -0,0 +1,8 @@
+// 객체. 메서트(매개변수);
+// object.method(parameter);
+console.log("메세지 출력"); //콘솔 영역에 출력
+console.debug("log와 마찬가지로 콘솔 영역에 출력");
+console.clear();
+
+//문서에 hello JavaScript를 작성
+document.querySelector("body").innerHTML = "Hello JavaScript";
