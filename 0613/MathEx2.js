9d16723b8bd2c3acc29e13b0aa055922f625b3a5
goeunjib <goeunji0948@naver.com>
2023-06-13 17:56:58 +0900

    0613

---
+++ 0613/MathEx2.js
@@ -0,0 +1,26 @@
+/* const btn = document.querySelector('#btn');
+document.querySelector('#btn').click(function() {});
+document.querySelector('#btn').onfullscreenchange('click', function() {}); */
+
+const btn = document.querySelector('#btn');
+// btn.addEventListener('click', function () {});
+
+/* btn.addEventListener('click', function () {
+  const red = Math.trunc(Math.random() * 255);
+  const green = Math.floor(Math.random() * 256);
+  const blue = Math.trunc(Math.random() * 255);
+  const color = `rgb(${red},${green},${blue})`;
+  console.log(red, green, blue);
+  document.body.style.backgroundColor = color;
+}); */
+
+const bgColor = () => {
+  const red = Math.trunc(Math.random() * 255);
+  const green = Math.floor(Math.random() * 256);
+  const blue = Math.trunc(Math.random() * 255);
+  const color = `rgb(${red},${green},${blue})`;
+  console.log(red, green, blue);
+  document.body.style.backgroundColor = color;
+};
+
+btn.addEventListener('click', bgColor);
