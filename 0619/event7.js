ab8aae41e710c8e2c17da46f707a1c875ec072b7
goeunjib <goeunji0948@naver.com>
2023-06-19 17:56:24 +0900

    0619

---
+++ 0619/event7.js
@@ -0,0 +1,40 @@
+/* 
+  event.ratget
+    실제 이벤트가 발생하는 대상 반환. 이벤트 리스너가 등록되어 있는 자식 요소 반환
+
+  event.currentTarget
+    이벤트 리스너가 있는 대상 반환. 이벤트로 등록해 놓은 요소(일반적으로는 버튼)
+  */
+
+const box1 = document.querySelector('.box1');
+const box2 = document.querySelector('.box2');
+const ul = document.querySelector('ul');
+const li = document.querySelector('li');
+const link = document.querySelector('a');
+
+// box1에 이벤트 binding
+box1.addEventListener('click', (evt) => {
+  console.log(`target -> ${evt.target.tagName}`);
+  console.log(`currentTarget -> ${evt.currentTarget.tagName}`);
+});
+/* 
+  box1 클릭 : target -> 실제 클릭한 노드 div, currentTarget -> 버튼으로 등록된 노드 div 
+  txt1 클릭 : target -> 실제 클릭한 노드 h3, currentTarget -> 버튼으로 등록된 노드 div
+  txt2 클릭 : target -> 실제 클릭한 노드 p, currentTarget -> 버튼으로 등록된 노드 div
+*/
+
+box2.addEventListener('click', (e) => {
+  e.preventDefault(); // 노드가 가진 성격제거
+
+  console.log(`target -> ${e.target.tagName}`);
+  console.log(`currentTarget -> ${e.currentTarget.tagName}`);
+
+  // e.target.style.backgroundColor = 'tomato'
+  const target = e.target;
+  target.style.borderColor = 'tomato';
+});
+/* 
+  이벤트를 바인딩한 요소는 box2
+  a 클릭: target -> 실제 클릭한 노드 a, currentTarget -> 버튼으로 등록된 노드 div 
+  li 클릭: target -> 실제 클릭한 노드 li, currentTarget -> 버튼으로 등록된 노드 div 
+*/
