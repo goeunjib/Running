b945d7001c1b3a50b0ac0fb6e698af8e7524dee3
goeunjib <goeunji0948@naver.com>
2023-06-21 17:56:49 +0900

    0621

--- 0620/traversing.js
+++ 0620/traversing.js
@@ -1,10 +1,12 @@
 /* 
+  https://api.jquery.com/category/traversing
+
   $('대상').method(); 대상의 값은 css에서 사용되던 선택자 작성 방식과 동일
 
-  == document.querySelector('');
+  == document.querySeletor('');
   jQuery('*'); 제이쿼리 방식으로 전체 선택자를 탐색해라
   $('#id'); 아이디 선택자 탐색
-  $('.class'); 클래스 선택자 탐색
+  $('.class'); 클래식 선택자 탐색
   $('#id > .class, type type'); 다중 선택자 탐색
   $('type:nth-child()');
   $('type[attribute=value]');
@@ -18,7 +20,7 @@
   $().prev(); 바로 이전 형제 노드 탐색
   $().next(); 바로 다음 형제 노드 탐색
   $().prevAll(); 이전 형제 노드 '모두' 탐색
-  $().nextAll(); 다음 형제 노드'모두' 탐색
+  $().nextAll(); 다음 형제 노드 '모두' 탐색
   $().first(); 일치하는 요소에서 첫번째 노드 탐색
   $().last(); 일치하는 요소에서 마지막 노드 탐색
   $().find(); 일치하는 하위 노드 탐색. 반드시 매개변수 작성해야 함
@@ -29,25 +31,57 @@
 // $(document).ready(function(){});
 
 $(document).ready(function () {
-  // ==.style.backgroundColor
-  // $().css({'속성': '값', '속성':'값'});
+  // == .style.backgroundColor
+  // $().css({'속성':'값', '속성':'값'});
   $('body').css({ 'background-color': '#efefef' });
-  $('body *').css({ border: '3px sold #999', padding: '10px', margin: '10' });
+  $('body *').css({ border: '3px solid #999', padding: '10px', margin: 10 });
 
-  $('a').parent(); // a의 직계 부모 (li) 탐색
+  $('a').parent(); // a의 직계 부모(li) 탐색
   // method chaining 여러 개의 메서드 연결
   $('a').parent().css({ 'border-color': 'red' });
-  // $('a').css({ 'border-color': 'red' }).parent(); a에 css를 적용한 뒤 부모 탐색
+  // $('a').css({'border-color':'red'}).parent(); a에 css를 적용한 뒤 부모 탐색
 
-  // $('a').parents().css({'border-style':'dotted'}); 매개변수를 작성하지 않으면 a의 상위 노드 모두 탐색. 즉 body, html도 탐색
+  // $('a').parents().css({'border-style': 'dotted'}); 매개변수를 작성하지 않으면 a의 상위 노드 모두 탐색. 즉 body, html도 탐색
   $('a').parents('#wrapper').css({ 'border-style': 'dotted' });
 
-  $('.container').children(); // 자식 노드 탐색. 매개 변수를 지정하지 않으면 같은 라인의 모든 직계 자식 노드 탐색
+  $('.container').children(); // 자식 노드 탐색. 매개변수를 지정하지 않으면 같은 라인의 모든 직계 자식 노드 탐색
   // $('.container').children().css({'border-color':'#000'})
   $('.container').children('h1').css({ 'border-color': '#000' });
 
   $('li.part').siblings(); // li.part의 형제 관계 탐색
-  $('li.part').siblings().css({
-    backgroundColor: 'lightblue',
+  $('li.part').siblings().css({ backgroundColor: 'lightblue' });
+  // $('li.part').siblings('li:last-child').children('a:first-child').css({ borderStyle: 'dashed' });
+
+  // .first(); 자식인 a들 중에 첫번째 탐색한 노드 반환
+  $('li.part').siblings('li:last-child').children().first().css({
+    borderStyle: 'dashed',
+    textDecoration: 'none',
+    color: 'red',
   });
+
+  $('.p2').prev(); // .p2의 앞에 작성된 형제 노드 탐색
+  $('.p2').prev().css({ backgroundColor: 'salmon' });
+
+  $('.p2').next(); // .p2의 뒤에 작성된 형제 노드 탐색
+  $('.p2').next().css({ backgroundColor: 'orange' });
+
+  $('.p2').prevAll(); // .p2의 앞에 작성된 형제 노드 *전부* 탐색
+  $('.p2').prevAll().css({ border: '6px groove #999' });
+
+  // text4 탐색
+  // $('#header').siblings('ul').children('li:last-child').children('a:last-child').css({ color:'#fff', textDecoration:'none', backgroundColor:'#000'});
+  // $('#header').siblings('ul').children('li:last-child').children().last().css({ color:'#fff', textDecoration:'none', backgroundColor:'#000'});
+
+  // .find('a:last-child'); ul의 하위 노드 중 a:last-child 탐색. 반드시 *매개변수* 필요
+  $('#header')
+    .siblings('ul')
+    .find('a:last-child')
+    .css({ color: '#fff', textDecoration: 'none', backgroundColor: '#000' });
+
+  $('#header').find('p').css({ backgroundColor: 'teal' });
+
+  $('.part').filter('ul'); // .part 중 ul 탐색
+  $('.part').filter('li').css({ listStyle: 'none' });
+
+  $('div').filter('#header').children().css({ padding: 40 });
 });
