a21b2f22c00ff08b462876bd7fd23cd2a6656551
goeunjib <goeunji0948@naver.com>
2023-06-22 17:56:46 +0900

    0622

---
+++ 0622/slideEx1.js
@@ -0,0 +1,26 @@
+/* // 대메뉴에 마우스를 올리면, 소메뉴가 내려옴
+$('.global_nav'); // 탐색
+
+$('.global_nav > li > a').on('mouseenter', () => {
+  $('nav .global_nav .sub_nav').slideDown();
+});
+$('.global_nav > li > a').on('mouseleave', () => {
+  $('nav .global_nav .sub_nav').slideUp();
+}); 
+
+$('.global_nav > li > a').on({
+  'mouseenter': () => {
+    $('nav .global_nav .sub_nav').slideDown();
+  }
+  'mouseleave': () => {
+    $('nav .global_nav .sub_nav').stop.slideUp();
+  }
+});
+*/
+
+$('.global_nav > li > a').on('mouseenter', () => {
+  $('nav .global_nav .sub_nav').slideDown();
+});
+$('.global_nav').on('mouseleave', () => {
+  $('nav .global_nav .sub_nav').slideUp();
+});
